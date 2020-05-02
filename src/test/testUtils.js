cb787bc3576f292b496cd6b3efe517b20edcecc9
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-05-02 11:15:16 -0400

    Add test utils methods, add check and prop-types

---
+++ src/test/testUtils.js
@@ -0,0 +1,30 @@
+import checkPropTypes from 'check-prop-types';
+
+
+/**
+ * Returns node(s) with the given data-test attribute.
+ *
+ * @param {ShallowWrapper} - Enzyme shallow wraper
+ * @param {string} val - Value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+}
+
+
+
+/**
+ * Factory function to check proptypes of component
+ * @param component
+ * @param conformingProps - { object of props name }
+ * @returns {CheckError}
+ */
+export const checkProps = (component, conformingProps) => {
+  return checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name,
+  );
+}

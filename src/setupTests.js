8b16c929ec26027545b1ddc9cd2e69c2b7e2c552
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-05-02 10:35:27 -0400

    Remove unneded files, setup tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import 'enzyme-adapter-react-16'
+
+import Enzyme from 'enzyme';
+import EnzymeAdaptor from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdaptor() });

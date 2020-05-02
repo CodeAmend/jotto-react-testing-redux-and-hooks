cc022c7d5b8bd5ab6c90f9b5351f97d4d72cd36e
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-05-02 10:22:49 -0400

    Initial Commit

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import EnzymeAdaptor from 'enzyme-adapter-react-16';
+
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdaptor() });
+
+test('renders learn react link', () => {
+
+})
